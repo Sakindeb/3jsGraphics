768a6fab915f5e2de3dac00ac1a23c5ef149b749
Sakindeb <sakindeborah@gmail.com>
2023-08-06 20:51:18 +0300

    Added a 3D line

--- greencube.js/greencube.js
+++ greencube.js/greencube.js
@@ -1,12 +1,18 @@
 import * as THREE from 'three';
-
+//instance of scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+//instance of camere using perspectivecamera
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 50 );
 camera.position.set( 0, 0, 100 );
 camera.lookAt( 0, 0, 0 );
+//instance of renderer using WebGLRenderer
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+//create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
 //define the geometry
 const points = [];
@@ -16,22 +22,16 @@ points.push( new THREE.Vector3( 10, 0 , 0 ) );
 
 const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
-//create a blue LineBasicMaterial
-const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-const line = new THREE.Line( geometry, material);
-scene.add( line );
+const line = new THREE.line( geometry, material);
+scene.add(line);
+function animate()
+{
+    requestAnimationFrame(animate);
+    renderer.render( scene, camera);
 
-
-
-function animate() {
-	requestAnimationFrame( animate );
-    line.rotation.x += 0.01;
-	line.rotation.y += 0.01;
-
-
-	
-
-	renderer.render( scene, camera );
 }
 
 animate();
+
+
+
